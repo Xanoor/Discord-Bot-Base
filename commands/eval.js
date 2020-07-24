3a53831573f0d9248abdb9b133099e896d5d2c4d
Xanoor1 <62515103+Xanoor@users.noreply.github.com>
2020-07-24 22:20:21 +0200

    Add files via upload

---
+++ commands/eval.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+
+module.exports.run = async(client, message, args) => {
+
+    function clean(text) {
+        if (typeof(text) === "string")
+            return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+        else
+            return text;
+    }
+
+    if (message.author.id !== 'OWNER_ID') return;
+    try {
+        const code = args.join(" ");
+        let evaled = eval(code);
+
+        if (typeof evaled !== "string")
+            evaled = require("util").inspect(evaled);
+
+        message.channel.send(clean(evaled), { code: "xl" });
+    } catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+}
+
+module.exports.cmd = {
+    name: 'eval'
+}
