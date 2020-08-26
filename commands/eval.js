891021721e7d31c0ec9ce8b16a8f3c3ff22d06c2
Xanoor1 <62515103+Xanoor@users.noreply.github.com>
2020-08-26 21:59:35 +0200

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,25 +1,16 @@
 const Discord = require('discord.js')
 
 module.exports.run = async(client, message, args) => {
-
-    function clean(text) {
-        if (typeof(text) === "string")
-            return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-        else
-            return text;
-    }
-
-    if (message.author.id !== 'OWNER_ID') return;
+    if (message.author.id !== "YOUR_ID") return message.channel.send('Vous n\'avez pas de droit d\'éxécuter cette commande !')
     try {
-        const code = args.join(" ");
-        let evaled = eval(code);
+        let codein = args.join(" ");
+        let code = eval(codein);
 
-        if (typeof evaled !== "string")
-            evaled = require("util").inspect(evaled);
-
-        message.channel.send(clean(evaled), { code: "xl" });
-    } catch (err) {
-        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+        if (typeof code !== 'string')
+            code = require('util').inspect(code, { depth: 0 });
+       
+    } catch (e) {
+        message.channel.send(`\`\`\`js\n${e}\n\`\`\``);
     }
 }
 
