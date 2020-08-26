0fd5448e916010067e89111e92e9c1da205d2c5c
Xanoor1 <62515103+Xanoor@users.noreply.github.com>
2020-08-26 23:26:49 +0200

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 module.exports.run = async(client, message, args) => {
     if (message.author.id !== "YOUR_ID") return message.channel.send('Vous n\'avez pas de droit d\'éxécuter cette commande !')
     try {
-        let codein = args.join(" ");
+        let codein = args.slice(1).join(" ");
         let code = eval(codein);
 
         if (typeof code !== 'string')
